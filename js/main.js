7e111fe2081fbd6e2de5e9e4c56d2bca930136f9
Sumer Mixon <sumer@ximm.us>
2017-08-23 15:19:32 -0400

    up and running with KO

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
-console.log("It's ALIVE!!");
+function AppViewModel() {
 
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').focus()
-})
+	this.pages = [
+		{ imgSrc: '/img/transfer.svg', page: 'Transfer'},
+		{ imgSrc: '/img/start-work.svg', page: 'Start Work'},
+		{ imgSrc: '/img/inventory-levels.svg', page: 'Inventory Levels'},
+		{ imgSrc: '/img/damaged-goods.svg', page: 'Damaged Goods'},
+		{ imgSrc: '/img/will-call.svg', page: 'Will Call'}
+	];
+
+};
+
+var vm = new AppViewModel();
+ko.applyBindings(vm);
