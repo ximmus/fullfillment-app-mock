430d70172f2166c241c6a9af40c01dfde9cb71be
Sumer Mixon <sumer@ximm.us>
2017-08-23 17:01:17 -0400

    starting page navigation

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 function AppViewModel() {
 
-	this.pages = [
-		{ imgSrc: '/img/transfer.svg', page: 'Transfer'},
-		{ imgSrc: '/img/start-work.svg', page: 'Start Work'},
-		{ imgSrc: '/img/inventory-levels.svg', page: 'Inventory Levels'},
-		{ imgSrc: '/img/damaged-goods.svg', page: 'Damaged Goods'},
-		{ imgSrc: '/img/will-call.svg', page: 'Will Call'}
-	];
+	// Protect 'this' from being hijacked
+	var self = this;
+
+	self.pages = ko.observableArray([
+		{ imgSrc: '/img/transfer.svg',         page: 'Transfer',         pageId: 'transfer'},
+		{ imgSrc: '/img/start-work.svg',       page: 'Start Work',       pageId: 'start-work'},
+		{ imgSrc: '/img/inventory-levels.svg', page: 'Inventory Levels', pageId: 'inventory-levels'},
+		{ imgSrc: '/img/damaged-goods.svg',    page: 'Damaged Goods',    pageId: 'damaged-goods'},
+		{ imgSrc: '/img/will-call.svg',        page: 'Will Call',        pageId: 'will-call'}
+	]);
+
+	var pageHeader = {
+    	header: 'Bob'
+	};
+
+	self.returnId = function(page){
+		self.pages(page);
+		console.log(page.page);
+	}
 
 };
 
